fe106175ce2b30c088302863d76d1d3462f47b6c
osaad2019 <osaad2019@gmail.com>
2023-04-26 14:34:29 -0400

    added function to check accuracy of 3 prediction methods

--- csvFunctions.py
+++ csvFunctions.py
@@ -4,19 +4,19 @@ from csv import DictReader
 
 #Code used from:
 #https://www.geeksforgeeks.org/load-csv-data-into-list-and-dictionary-using-python/
-def genTeamDict():
+def genCSVDictList(csvName):
     dirPath = os.path.dirname(os.path.realpath(__file__))
-    teams = "teams.csv"
-    teams = os.path.join(dirPath, teams)
-    with open(teams, 'r') as f:
+    csvList = csvName
+    csvList = os.path.join(dirPath, csvList)
+    with open(csvList, 'r') as f:
         
         dict_reader = DictReader(f)
-        teamDict = list(dict_reader)
+        dictList = list(dict_reader)
     
     f.close()
-    return teamDict
+    return dictList
 
-def buildNewDict(teamDict):
+def buildNewGamesDictList(teamDict):
     dirPath = os.path.dirname(os.path.realpath(__file__))
     games = "games.csv"
     games = os.path.join(dirPath, games)
@@ -71,7 +71,53 @@ def writeDictToCSV(fieldnames, csvName, dictList):
         for element in dictList:
             writer.writerow(element)
 
-fieldnames = ['Date', 'Home_Team_Name', 'Vistor_Team_Name', 'Home_Points', 'Away_Points']
-teamDict = genTeamDict()
-newDictList = buildNewDict(teamDict)
-writeDictToCSV(fieldnames, "newGames.csv", newDictList)
+def buildStatsDictList(predScores):
+    statsDictList = []
+    count = 0
+    RFCount = 0
+    LRCount = 0
+    NNCount = 0
+    for element in predScores:
+        if element['TrueHome'] > element['TrueAway']:       #Home Team Acutally wins
+            if element['RFHome'] > element['RFAway']:       #randomForest predicts Home team wins
+                RFCount +=  1
+            if element['LRHome'] > element['LRAway']:       #LinearRegression predicts Home team wins
+                LRCount +=  1
+            if element['NNHome'] > element['NNAway']:       #Neural Network predicts Home team wins
+                NNCount +=  1
+        else:                                               #Away team actually wins
+            if element['RFAway'] > element['RFHome']:       #randomForest predicts away team wins
+                RFCount +=  1
+            if element['LRAway'] > element['LRHome']:       #LinearRegression predicts Home team wins
+                LRCount +=  1 
+            if element['NNAway'] > element['NNHome']:       #Neural Network predicts Home team wins
+                NNCount +=  1 
+        count += 1
+    
+    print("Games Assessed: " + str(count))
+    print("randomForest correct games: " + str(RFCount) + " Accuracy: " + str(((RFCount/count)*100)) + "%")
+    print("LinearRegression correct games: " + str(LRCount) + " Accuracy: " + str(((LRCount/count)*100)) + "%")
+    print("Neural Network correct games: " + str(NNCount) + " Accuracy: " + str(((NNCount/count)*100)) + "%")
+
+        
+    return
+    #return statsDictList
+
+predScores = genCSVDictList("True-RF-LR-NN_Performance_Scores.csv")
+buildStatsDictList(predScores)
+''' 
+'' = index
+0 = real home score
+1 = real away score
+2 = randomForest home score
+3 = randomForest away score
+4 = LinearRegression home score
+5 = LinearRegression away score
+'''
+
+
+#print(predScores)
+#fieldnames = ['Date', 'Home_Team_Name', 'Vistor_Team_Name', 'Home_Points', 'Away_Points']
+#teamDict = genTeamDict()
+#newDictList = buildNewDict(teamDict)
+#writeDictToCSV(fieldnames, "newGames.csv", newDictList)
