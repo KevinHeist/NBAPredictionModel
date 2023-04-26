cd5b42274189642a85e0c4c6ca4a4b6c58513ac9
osaad2019 <osaad2019@gmail.com>
2023-04-26 15:10:35 -0400

    Updated accuracy check function, fixed string to float error and added home/away accuracy

--- csvFunctions.py
+++ csvFunctions.py
@@ -71,40 +71,64 @@ def writeDictToCSV(fieldnames, csvName, dictList):
         for element in dictList:
             writer.writerow(element)
 
-def buildStatsDictList(predScores):
+def accuracyCheck(predScores):
     statsDictList = []
-    count = 0
-    RFCount = 0
-    LRCount = 0
-    NNCount = 0
+    countA = 0
+    countH = 0
+    RFCountH = 0
+    LRCountH = 0
+    NNCountH = 0
+    RFCountA = 0
+    LRCountA = 0
+    NNCountA = 0
     for element in predScores:
-        if element['TrueHome'] > element['TrueAway']:       #Home Team Acutally wins
-            if element['RFHome'] > element['RFAway']:       #randomForest predicts Home team wins
-                RFCount +=  1
-            if element['LRHome'] > element['LRAway']:       #LinearRegression predicts Home team wins
-                LRCount +=  1
-            if element['NNHome'] > element['NNAway']:       #Neural Network predicts Home team wins
-                NNCount +=  1
-        else:                                               #Away team actually wins
-            if element['RFAway'] > element['RFHome']:       #randomForest predicts away team wins
-                RFCount +=  1
-            if element['LRAway'] > element['LRHome']:       #LinearRegression predicts Home team wins
-                LRCount +=  1 
-            if element['NNAway'] > element['NNHome']:       #Neural Network predicts Home team wins
-                NNCount +=  1 
-        count += 1
-    
-    print("Games Assessed: " + str(count))
-    print("randomForest correct games: " + str(RFCount) + " Accuracy: " + str(((RFCount/count)*100)) + "%")
-    print("LinearRegression correct games: " + str(LRCount) + " Accuracy: " + str(((LRCount/count)*100)) + "%")
-    print("Neural Network correct games: " + str(NNCount) + " Accuracy: " + str(((NNCount/count)*100)) + "%")
+        if int(element['TrueHome']) > int(element['TrueAway']):           #Home Team Acutally wins
+            if float(element['RFHome']) > float(element['RFAway']):       #randomForest predicts Home team wins
+                RFCountH +=  1
+            if float(element['LRHome']) > float(element['LRAway']):       #LinearRegression predicts Home team wins
+                LRCountH +=  1
+            if float(element['NNHome']) > float(element['NNAway']):       #Neural Network predicts Home team wins
+                NNCountH +=  1
+            countH += 1
+        elif int(element['TrueAway']) > int(element['TrueHome']):         #Away team actually wins
+            if float(element['RFAway']) > float(element['RFHome']):       #randomForest predicts away team wins
+                RFCountA +=  1
+            if float(element['LRAway']) > float(element['LRHome']):       #LinearRegression predicts Home team wins
+                LRCountA +=  1 
+            if float(element['NNAway']) > float(element['NNHome']):       #Neural Network predicts Home team wins
+                NNCountA +=  1 
+                #print(element)
+            countA += 1
+    RFHacc = "{:.2f}".format((RFCountH/countH)*100)
+    RFAacc = "{:.2f}".format((RFCountA/countA)*100)
+    RFacc = "{:.2f}".format(((RFCountA + RFCountH)/(countA + countH))*100)
+    LRHacc = "{:.2f}".format((LRCountH/countH)*100)
+    LRAacc = "{:.2f}".format((LRCountA/countA)*100)
+    LRacc = "{:.2f}".format(((LRCountA + LRCountH)/(countA + countH))*100)
+    NNHacc = "{:.2f}".format((NNCountH/countH)*100)
+    NNAacc = "{:.2f}".format((NNCountA/countA)*100)
+    NNacc = "{:.2f}".format(((NNCountA + NNCountH)/(countA + countH))*100)
 
+    print("Games Assessed: " + str((countA + countH)))
+    print()
+    print("randomForest home correct games: " + str(RFCountH) + "\t\taway correct games: " + str(RFCountA))
+    print("randomForest home accuracy: " + str(RFHacc) + "%\t\taway accuracy: " + str(RFAacc) + "%")
+    print("randomForest correct games: " + str(RFCountA + RFCountH) + "\t\t\tAccuracy: " + str(RFacc) + "%")
+    print()
+    print("LinearRegression home correct games: " + str(LRCountH) + "\taway correct games: " + str(LRCountA))
+    print("LinearRegression home accuracy: " + str(LRHacc) + "%\t\taway accuracy: " + str(LRAacc) + "%")
+    print("LinearRegression correct games: " + str(LRCountA + LRCountH) + "\t\tAccuracy: " + str(LRacc) + "%")
+    print()
+    print("Neural Network home correct games: " + str(NNCountH) + "\t\taway correct games: " + str(NNCountA))
+    print("Neural Network home accuracy: " + str(NNHacc) + "%\t\taway accuracy: " + str(NNAacc) + "%")
+    print("Neural Network correct games: " + str(NNCountA + NNCountH) + "\t\tAccuracy: " + str(NNacc) + "%")
+    
         
     return
     #return statsDictList
 
 predScores = genCSVDictList("True-RF-LR-NN_Performance_Scores.csv")
-buildStatsDictList(predScores)
+accuracyCheck(predScores)
 ''' 
 '' = index
 0 = real home score
